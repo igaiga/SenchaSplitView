e317980519a951d185fdd921eaff8b6f102c1eba
Kuniaki IGARASHI <igaiga@gmail.com>
2010-08-04 19:23:53 +0900

    add ifiles

---
+++ src/structure.js
@@ -0,0 +1,69 @@
+sink.Structure = [{
+    text: 'User Interface',
+    cls: 'launchscreen',
+    items: [{
+        text: 'Buttons',
+        card: demos.Buttons,
+        source: 'src/demos/buttons.js'
+    },
+    {
+        text: 'Forms',
+        card: demos.Forms,
+        source: 'src/demos/forms.js'
+    },
+    {
+        text: 'List',
+        card: demos.List,
+        source: 'src/demos/list.js'
+    },
+    {
+        text: 'Icons',
+        card: demos.Icons,
+        source: 'src/demos/icons.js'
+    },
+    {
+        text: 'Toolbars',
+        card: demos.Toolbars,
+        source: 'src/demos/toolbars.js'
+    },
+    {
+        text: 'Carousel',
+        card: demos.Carousel,
+        source: 'src/demos/carousel.js'
+    },
+    {
+        text: 'Tabs',
+        card: demos.Tabs,
+        source: 'src/demos/tabs.js'
+    },
+    {
+        text: 'Bottom Tabs',
+        card: demos.BottomTabs,
+        source: 'src/demos/bottomtabs.js'
+    },
+    /*{
+        text: 'Picker',
+        card: demos.Picker,
+        source: 'src/demos/picker.js'
+    },*/
+    {
+        text: 'Map',
+        card: demos.Map,
+        source: 'src/demos/map.js'
+    },
+    {
+        text: 'Overlay',
+        card: demos.Overlay,
+        source: 'src/demos/overlay.js'
+    }]
+},
+{
+    text: 'Events',
+    card: demos.Touch,
+    source: 'src/demos/touch.js'
+},
+{
+    text: 'Data',
+    card: demos.Data,
+    source: 'src/demos/data.js'
+}];
